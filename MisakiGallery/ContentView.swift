82d36b556fe7a83943e1ee13df03b4f4bb7ba3f3
Spike Liao <lpylpyleo@gmail.com>
2021-12-17 09:54:45 +0800

    Initial Commit

---
+++ MisakiGallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MisakiGallery
+//
+//  Created by Spike Liao on 2021/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MisakiGallery/MisakiGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisakiGalleryApp.swift
+//  MisakiGallery
+//
+//  Created by Spike Liao on 2021/12/17.
+//
+
+import SwiftUI
+
+@main
+struct MisakiGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
